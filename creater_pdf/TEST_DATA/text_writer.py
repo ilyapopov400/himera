872318c4c099410828fc711264475434f73f2634
ilya_popov <il_pop@mail.ru>
2024-04-22 15:27:57 +0300

    ошибки в файле credit, phone

--- creater_pdf/TEST_DATA/text_writer.py
+++
@@ -1,6 +0,0 @@
-import json
-
-if __name__ == "__main__":
-    with open("credit", "r") as my_file:
-        result = json.loads(my_file.read())
-        print(result)
--- creater_pdf/creater_pdf.py
+++ creater_pdf/creater_pdf.py
@@ -30,42 +30,51 @@ class CreatorPDF:
         :param text_json:
         :return: list
         """
-        if "error" in text_json.keys() or text_json.get("status") != "ok":
-            return False
+        # if "error" in text_json.keys() or text_json.get("status") != "ok":
+        #     return [{"Нет данных": ""}, ]
 
-        return text_json.get("data")
+        if text_json.get("status") == "ok":
+            return text_json.get("data")
+        else:
+            result = list()
+            result.append(text_json)
+            return result
 
-    def creator_pdf(self):
-        file = self.file_list[9]
+    def creator_pdf(self, file):
         text_json = self._get_data(path_file=file)
         result = self._parser(text_json=text_json)
 
         text = list()
+        text.append("HIMERA")
         for row in result:
             for key in row.keys():
-                # print(key, row.get(key))
                 text.append(
-                    "{}: {}\n".format(key, row.get(key))
+                    "{}: {}".format(key, row.get(key))
                 )
 
-        # print(text)
         pdf = FPDF()
         pdf.add_page()
         pdf.add_font("Arial", "", "arialmt.ttf", uni=True)
         pdf.set_font("Arial", size=14)
-        # text = pdf.normalize_text(txt=text)  # TODO
-        # text.encode(encoding='UTF-8', errors='strict')
         for row in text:
             pdf.cell(200, 10, txt=row, ln=1, align='C')
-        # pdf.cell(200, 10, txt=text.encode('utf-8').decode('latin-1'), ln=1, align='C')
+
         # Сохраняем созданный PDF-файл
         pdf.output(name="data.pdf")
         return result
 
+    def __call__(self):
+        for e, file in enumerate(self.file_list):
+            try:
+                self.creator_pdf(file=file)
+            except:
+                print(e, file)
+        number_file = 8
+        print(self.file_list[number_file])
+        file = self.file_list[number_file]  # TODO для одного файла, далее циклом по всем
+        self.creator_pdf(file=file)
+
 
 if __name__ == "__main__":
     path = "TEST_DATA"
-    result = CreatorPDF(path=path).creator_pdf()
-    # print(len(result))
-    # for key, value in result[0].items():
-    #     print(key, ": ", value)
+    result = CreatorPDF(path=path)()
